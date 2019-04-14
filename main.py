487b3813402be9ade118989a6829ee7c665cf531
Marius <mrs.parasca@gmail.com>
2019-04-14 19:49:41 +0300

    Added synonyms features

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from nltk.corpus import wordnet
 from nltk.corpus import sentiwordnet as swn
 from nltk.corpus.reader.wordnet import WordNetError
 # from collections import defaultdict
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
@@ -12,7 +12,6 @@ from sklearn.metrics import accuracy_score
 from sklearn.utils import shuffle
 from scipy.sparse import csr_matrix, hstack
 from pymetamap import MetaMap
-import numpy as np
 import re
 import string
 
@@ -26,6 +25,7 @@ class Preprocesor:
     NOUN = 'NN'
     VERB = 'VB'
     ADVERB = 'RB'
+    SYN_TAG = "SYN"
 
     def __init__(self, metamap_path='/home/noway/Facultate/Licenta/public_mm/bin/metamap18'):
         self.mm = MetaMap.get_instance(metamap_path)
@@ -33,7 +33,25 @@ class Preprocesor:
         self.y = []
         self.corpus = []
         self.umls_semtypes_cuis = []
+        self.syns_features = []
         self.sem_abbreviation_translations = []
+
+    @staticmethod
+    def train_fit_with_naive_bayes(x_train, y_train):
+        model = MultinomialNB()
+        model.fit(x_train, y_train)
+        return model
+
+    @staticmethod
+    def train_fit_with_svc(x_train, y_train, kernel='linear', random_state=0):
+        model = SVC(kernel=kernel, random_state=random_state)
+        model.fit(x_train, y_train)
+        return model
+
+    @staticmethod
+    def test_model(model, model_name, x_test, y_test):
+        y_predicted = model.predict(x_test)
+        print("Accuracy " + model_name + ": ", accuracy_score(y_test, y_predicted))
 
     @staticmethod
     def metamap_pos_to_sentiwordnet_pos(pos):
@@ -47,15 +65,6 @@ class Preprocesor:
             return "r"
         else:
             return ""
-
-    def ntlk_pos(self, s, string_tokenezed=True, to_stem=True, to_string=False):
-        if to_stem:
-            s = self.get_nltk_porter_stemming(word_tokenize(s), to_string=to_string)
-        elif string_tokenezed:
-            pass
-        else:
-            s = word_tokenize(s.lower())
-        return pos_tag(s)
 
     @staticmethod
     def get_nltk_porter_stemming(tokens, to_string=True):
@@ -88,7 +97,7 @@ class Preprocesor:
     def get_n_grams_words(tokenized_string, n):
         """"""
         return list(ngrams(tokenized_string, n))
-    
+
     @staticmethod
     def tokenize_for_tfidf(text):
         tokens = word_tokenize(text)
@@ -97,31 +106,49 @@ class Preprocesor:
             stems.append(PorterStemmer().stem(item))
         return stems
 
-    def transform_data_to_numpy_array(self):
-        self.x = np.array(self.x)
-        self.y = np.array(self.y)
-
-    def concat_x_with(self, data):
-        np.concatenate((self.x, data), axis=1)
-
-    def n_grams_fit_transform(self, ngram_range=(1, 3)):
-        vectorizer = CountVectorizer(ngram_range=ngram_range)
-        self.x = vectorizer.fit_transform(self.corpus)
-
-    def tfidf_transformer_fit_traform(self):
-        tfidf_transformer = TfidfTransformer()
-        x = tfidf_transformer.fit_transform(self.x)
-        return x
-
-    def tfidf_fit_transform(self):
-        tfidf = TfidfVectorizer(tokenizer=Preprocesor.tokenize_for_tfidf, stop_words='english')
-        tfidf.fit_transform(self.corpus)
-        print(tfidf)
-        return tfidf
-
     @staticmethod
     def delete_punctuation(text):
         return text.lower().translate(str.maketrans('', '', string.punctuation))
+
+    @staticmethod
+    def get_synonyms(word):
+        synonyms = set()
+        for syn in wordnet.synsets(word):
+            for l in syn.lemmas():
+                synonyms.add(l.name() + Preprocesor.SYN_TAG)
+        return list(synonyms)
+
+    @staticmethod
+    def get_syn_set(pos_text):
+        result = []
+        for word, pos_word in pos_text:
+            if Preprocesor.NOUN in pos_word or Preprocesor.ADJ in pos_word or Preprocesor.VERB in pos_word:
+                result += Preprocesor.get_string_from_syn_set(word)
+        return result
+
+    @staticmethod
+    def get_string_from_syn_set(word):
+        aux = Preprocesor.get_synonyms(word)
+        if not aux:
+            return ""
+        else:
+            return aux
+
+    def ntlk_pos(self, s, string_tokenezed=True, to_stem=True, to_string=False):
+        if to_stem:
+            s = self.get_nltk_porter_stemming(word_tokenize(s), to_string=to_string)
+        elif string_tokenezed:
+            pass
+        else:
+            s = word_tokenize(s.lower())
+        return pos_tag(s)
+
+    def concat_x_with(self, data, frmt='csr'):
+        self.x = hstack([self.x, data], format=frmt)
+
+    def create_n_grams(self, ngram_range=(1, 3)):
+        vectorizer = CountVectorizer(ngram_range=ngram_range)
+        self.x = vectorizer.fit_transform(self.corpus)
 
     def read_rel_extension_file(self, filepath):
         with open(filepath, 'r') as fd:
@@ -140,14 +167,7 @@ class Preprocesor:
     def shuffle_data(self):
         self.x, self.y = shuffle(self.x, self.y)
 
-    def test_for_good_shuffle(self):
-        count = 0
-        for i in range(0, len(self.x)):
-            if self.x[i][len(self.x) - 1] == self.y[0]:
-                count += 1
-        print(count, len(self.x))
-
-    def get_concept(self, text_array):  # tfidf mai trebuie facut =-----------------------------------------
+    def get_concept(self, text_array):
         concepts, error = self.mm.extract_concepts(text_array)
         semantic_types = set()
         cuis = set()
@@ -160,28 +180,6 @@ class Preprocesor:
                 semantic_types.add(semantic_type)
             cuis.add(concept.cui)
         return list(semantic_types), list(cuis)
-
-    @staticmethod
-    def get_synonyms(word):
-        synonyms = set()
-        for syn in wordnet.synsets(word):
-            for l in syn.lemmas():
-                synonyms.add(l.name())
-        return list(synonyms)
-
-    @staticmethod
-    def get_syn_set(pos_text):  # tfidf mai trebuie facut =-----------------------------------------
-        result = []
-        for word, pos_word in pos_text:
-            if Preprocesor.NOUN in pos_word:
-                result.append(list(set([word] + Preprocesor.get_synonyms(word))) + [pos_word])
-            elif Preprocesor.ADJ in pos_word:
-                result.append(list(set([word] + Preprocesor.get_synonyms(word))) + [pos_word])
-            elif Preprocesor.VERB in pos_word:
-                result.append(list(set([word] + Preprocesor.get_synonyms(word))) + [pos_word])
-            else:
-                result.append([word, pos_word])
-        return result
 
     def get_sentiment_score(self, text):  # nu stiu daca e corect si bun cum am facut
         sentence_pos = self.ntlk_pos(text, to_stem=False)
@@ -202,51 +200,8 @@ class Preprocesor:
                     pass
         return sum_pos_socre / len(sentence_pos), sum_neg_score / len(sentence_pos), sum_score / len(sentence_pos)
 
-    # def get_features(self):
-    #     features = defaultdict(list)
-    #     self.transform_data_to_numpy_array()
-    #
-    #     # for data in self.x:
-    #     #     break
-    #     # tokenized_string = Preprocesor.get_string_tokenizing(data[1])  # word_tokenize(data[1])
-    #     # print(tokenized_string)
-    #     # features["1-grams"].append(Preprocesor.get_n_grams_words(tokenized_string, 1))
-    #     # features["2-grams"].append(Preprocesor.get_n_grams_words(tokenized_string, 2))
-    #     # features["3-grams"].append(Preprocesor.get_n_grams_words(tokenized_string, 3))
-    #     # features["semantic"].append(self.get_concept([data[1]]))
-    #     # sentences_pos = self.ntlk_pos(tokenized_string, string_tokenezed=True, to_stem=False)
-    #     # features["sentence_pos"].append(sentences_pos)
-    #     # features["synset"].append(Preprocesor.get_syn_set(sentences_pos))
-    #     # features["sentiment-score"].append(self.get_sentiment_score(data[1]))
-    #     # features["id"].append(data[0])
-    #     # features["stemmed-text"].append(Preprocessor.get_nltk_porter_stemming(tokenized_string))
-    #
-    #     return features
-
-    def write_n_grams_to_file(self, filename, data):
-        with open(filename, 'w') as fd:
-            for i, x in enumerate(self.x):
-                fd.write(" ".join(np.concatenate((x, data[i]))) + '\n')
-
     def split_train_test(self, test_size=0.25, random_state=0):
         return train_test_split(self.x, self.y, test_size=test_size, random_state=random_state)
-
-    @staticmethod
-    def train_fit_with_naive_bayes(x_train, y_train):
-        model = MultinomialNB()
-        model.fit(x_train, y_train)
-        return model
-
-    @staticmethod
-    def train_fit_with_svc(x_train, y_train, kernel='linear', random_state=0):
-        model = SVC(kernel=kernel, random_state=random_state)
-        model.fit(x_train, y_train)
-        return model
-
-    @staticmethod
-    def test_model(model, model_name, x_test, y_test):
-        y_predicted = model.predict(x_test)
-        print("Accuracy " + model_name + ": ", accuracy_score(y_test, y_predicted))
 
     def create_sem_abbreviation_translations(self, file_path=r'./abreviations_files/SemanticTypes_2018AB.txt'):
         with open(file_path, 'r') as fd:
@@ -275,16 +230,43 @@ class Preprocesor:
                 sem_types, cuis = self.get_concept([text])
                 aux = self.translate_semantic_abbreviation(sem_types)
                 fd.write(" ".join(aux + cuis) + '\n')
+        print("UMLS features saved in:", filepath)
 
     def load_umls_features(self, filepath=r'./raw_features/semantic_types_ADE.txt'):
         with open(filepath, 'r') as fd:
             for line in fd.readlines():
                 self.umls_semtypes_cuis.append(line.rstrip())
+        print("UMLS features loaded from:", filepath)
+
+    def load_syns_features(self, filepath=r'./raw_features/syns_ADE.txt'):
+        with open(filepath, 'r') as fd:
+            for line in fd.readlines():
+                self.syns_features.append(line.rstrip())
+        print("Synonym features loaded from:", filepath)
+
+    def save_syns_features(self, filepath=r'./raw_features/syns_ADE.txt'):
+        with open(filepath, 'w') as fd:
+            for line in self.corpus:
+                sentences_pos = self.ntlk_pos(line, to_stem=True)
+                synonyms = Preprocesor.get_syn_set(sentences_pos)
+                if not synonyms:
+                    fd.write("-\n")
+                else:
+                    fd.write(" ".join(synonyms) + "\n")
+        print("Synonym features saved in:", filepath)
 
     def create_concepts_file(self, filepath_to_save=r'./raw_features/semantic_types_ADE.txt',
                              filepath_to_abbr_file=r'./abreviations_files/SemanticTypes_2018AB.txt'):
         self.create_sem_abbreviation_translations(filepath_to_abbr_file)
         self.save_umls_features(filepath_to_save)
+
+    def create_tfidf_syns(self):
+        self.load_syns_features()
+        vectorizer = CountVectorizer()
+        x_aux = vectorizer.fit_transform(self.syns_features)
+        tfidf_transformer = TfidfTransformer()
+        x_to_stack = tfidf_transformer.fit_transform(x_aux)
+        self.concat_x_with(x_to_stack)
 
     def create_tfidf_umls(self):
         self.load_umls_features()
@@ -292,28 +274,33 @@ class Preprocesor:
         x_aux = vectorizer.fit_transform(self.umls_semtypes_cuis)
         tfidf_transformer = TfidfTransformer()
         x_to_stack = tfidf_transformer.fit_transform(x_aux)
-        self.x = hstack([self.x, x_to_stack], format='csr')
+        self.concat_x_with(x_to_stack)
 
     def create_features(self):
-        self.n_grams_fit_transform()
+        self.create_n_grams()
         self.create_tfidf_umls()
+        self.create_tfidf_syns()
 
     def train_model(self, model_name='naive_bayes'):
-        model = None
         self.create_features()
         self.shuffle_data()
         x_train, x_test, y_train, y_test = self.split_train_test()
+        print("Start training using", model_name)
         if model_name == 'naive_bayes':
-            model = Preprocesor.train_fit_with_naive_bayes(x_train, y_train)  # NB accuracy score: 0.8462323524408913
+            model = Preprocesor.train_fit_with_naive_bayes(x_train, y_train)  # NB accuracy score: 0.8462323524408913 ->
+            # 0.8617111753699609
         elif model_name == 'svc':
-            model = Preprocesor.train_fit_with_svc(x_train, y_train)  # SVM accuracy score: 0.9027045415887056
-
+            model = Preprocesor.train_fit_with_svc(x_train, y_train)  # SVM accuracy score: 0.9027045415887056  ->
+            # 0.9055961898282021
+        else:
+            raise Exception("Unknown model. Available models: naive_bayes, svc")
+        print("Finishing training")
         return model, x_test, y_test
 
 # 4.1.2. N-grams - done
 # 4.1.3. UMLS semantic types and concept IDs - done (nu stiu daca CUI-urile trebuie transformate cumva,
 #                                                    asa ca le-am lasat asa cum sunt ele)
-# 4.1.4. Syn-set expansion - half -> no tfidf -> Pe ce trebuie sa fac tdidf?
+# 4.1.4. Syn-set expansion - done
 # 4.1.5. Change phrases - neimplementat
 # 4.1.6. ADR lexicon matches - neimplementat
 # 4.1.7. Sentiword scores - done -> nu stiu daca e corect
@@ -333,7 +320,7 @@ def main():
     Preprocesor.test_model(model, model_name, x_test, y_test)
 
     # transform_data_to_numpy_array()
-    # p.n_grams_fit_transform()
+    # p.create_n_grams()
 
     # p.get_features()
     # p.get_features(dataset)
